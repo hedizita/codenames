690d33d41a12ba1fc0d99cd437614eec87b9dd4d
hedizita <115140400+hedizita@users.noreply.github.com>
2022-10-14 15:11:09 +0200

    nemtudom

--- codenames/__init__.py
+++ codenames/__init__.py
@@ -1,2 +1,3 @@
 from .prompts import prompt_for_teammate_after_response
 from .board import generate_codewords_board
+from .codemaster import prompt_for_codemaster
---
+++ codenames/codemaster.py
@@ -0,0 +1,15 @@
+def prompt_for_codemaster(board_cards, team):
+  opponent = {'red': 'blue', 'blue':'red'}
+  prompt = f"""You are playing Codenames, you are the Codemaster.
+Find clue word that is related to words from Group A but not related to words from Group B or the Bomb words or the Neutral words.
+
+Group A: {', '.join(cards[team])}.
+Group B: {', '.join(cards[opponent[team]])}.
+Neutral: {', '.join(cards['neutral'])}.
+Bomb words: {', '.join(cards['bomb'])}.
+
+The clue word must not be the same as any of the words in the Group A, Group B, Neutral or Bomb.
+You also give a number between 1 and 3, this is the number of words your clue relates to from group A.
+
+Your response (clue word and number separated by comma):"""
+  return prompt
