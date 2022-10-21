de009ce44e76a9c65057ed9a3f8ec1c71e5e9366
hedizita <115140400+hedizita@users.noreply.github.com>
2022-10-21 15:58:44 +0200

    added prompt_for_teammate

--- codenames/teammate.py
+++ codenames/teammate.py
@@ -6,3 +6,18 @@ def prompt_for_teammate_after_response(response):
 def parse_teammate_response(response_text):
   tokens = [token for token in re.split('[-, \n]', response_text) if token!='']
   return tokens[0]
+
+
+def prompt_for_teammate(cards, clue_word, number):
+  prompt = f"""You are playing the boardgame Codenames. You are the teammate.
+
+Your clue word is "{clue_word}". You must guess {number} of words. All of these words need to be different.
+
+The clue word relates to some of these words: {', '.join([', '.join(v) for v in cards.values()])}.
+
+First, you have to guess only one word. You must not guess a word which is not in the game.
+
+Your guess is: 
+"""
+
+  return prompt
