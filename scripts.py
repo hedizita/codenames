4de7483cc9044bf075df45424e4a5f7576254cb2
hedizita <kovbanhezi@gmail.com>
2022-12-15 16:21:09 +0100

    eztsetudom

--- scripts.py
+++ scripts.py
@@ -1,5 +1,21 @@
 import codenames
 from codenames.codemaster import prompt_for_codemaster
+import openai
+
 
 cards = codenames.generate_codewords_board(codenames.EASY_WORDS, 3, 3, 3, 1)
-prompt_for_codemaster(cards, "blue")
+codemaster_prompt = prompt_for_codemaster(cards, "blue")
+
+
+response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=codemaster_prompt,
+            temperature=0.7,
+            max_tokens=6,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+)
+
+clue_word, number = codenames.parse_codemaster_response(response['choices'][0]['text'])
+print(clue_word, number)
