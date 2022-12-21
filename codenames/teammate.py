d039a4abcb5a3c2f30774f2a4f6c69a8c32b3f60
fhuszar <ferenc.huszar@gmail.com>
2022-12-21 16:07:20 +0100

    shuffles cards before presenting options to teammate

--- codenames/teammate.py
+++ codenames/teammate.py
@@ -1,9 +1,16 @@
+from random import shuffle
+
 def prompt_for_teammate(cards, clue_word, number):
+    allcards = []
+    for l in cards.values():
+        for card in l:
+            allcards.append(card)
+    shuffle(allcards)
     prompt = f"""You are playing the boardgame Codenames. You are the teammate.
 
 Your clue word is "{clue_word}". You must guess {number} of words. All of these words need to be different.
 
-The clue word relates to some of these words: {', '.join([', '.join(v) for v in cards.values()])}.
+The clue word relates to some of these words: {', '.join(allcards)}.
 
 First, you have to guess only one word. You must not guess a word which is not in the game.
 
